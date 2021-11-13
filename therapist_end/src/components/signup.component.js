59d35dea0f740054fbe33cb7b95184b3808c9a55
Dhruv <dhruvsh19@gmail.com>
2021-11-12 23:32:56 -0500

    #1

---
+++ therapist_end/src/components/signup.component.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Sign Up</h3>
+
+                <div className="form-group">
+                    <label>I`m </label>
+                    &nbsp; &nbsp;&nbsp;
+                    <input type="radio" id="Therapist" name="who" value="Therapist"/>&nbsp; 
+                    <label for="Therapist">Therapist</label>
+                    &nbsp; &nbsp;&nbsp;
+                    <input type="radio" id="Patient" name="who" value="Patient"/>&nbsp; 
+                    <label for="Patient">Patient</label>
+
+                </div>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
+
+                
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+                <pre></pre>
+                
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="#">sign in?</a>
+                </p>
+            </form>
+        );
+    }
+}
