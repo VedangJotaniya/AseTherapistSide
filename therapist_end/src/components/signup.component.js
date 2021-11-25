3d8c22ddcfb4ba7c1d5ba202ab4198400b2e3a0c
Dhruv <dhruvsh19@gmail.com>
2021-11-25 15:50:13 -0500

    change

--- therapist_end/src/components/signup.component.js
+++ therapist_end/src/components/signup.component.js
@@ -2,6 +2,37 @@ import React, { Component } from "react";
 
 
 export default class SignUp extends Component {
+
+    constructor() {
+        super();
+        this.setState ={
+            FName: "",
+            LName: "",
+            email: "",
+            location: "",
+            password: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.Senddata = this.Senddata.bind(this);
+    }
+    Senddata = (event) => {
+        console.log(event);
+        console.log(this.state);
+        alert("Success");
+    }
+    
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        this.state = {
+          [name]: value
+        };
+      }
+    
+
+
     render() {
         return (
             <form>
@@ -20,7 +51,7 @@ export default class SignUp extends Component {
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input type="text" className="form-control" onChange={(e) => {this.handleChange(e)}}/>
                 </div>
 
                 <div className="form-group">
@@ -41,7 +72,7 @@ export default class SignUp extends Component {
                 </div>
                 <pre></pre>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <button type="submit" className="btn btn-primary btn-block" onClick={() => {this.Senddata()}}>Sign Up</button>
                 <p className="forgot-password text-right">
 
                     Already registered <a href="index.html">sign in?</a>
