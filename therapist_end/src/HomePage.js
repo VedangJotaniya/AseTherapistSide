e92805562063b79cb394c0cd8d9ce5041f5faa61
Dhruv <dhruvsh19@gmail.com>
2021-12-04 10:51:49 -0500

    4

--- therapist_end/src/HomePage.js
+++ therapist_end/src/HomePage.js
@@ -5,7 +5,7 @@ import Chat from './Chat';
 import Assignment from './Assignment';
 import FAQ from './FAQ';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import headimg from './image/img1.jpg';
 
 
 export default class HomePage extends React.Component {
@@ -37,6 +37,9 @@ export default class HomePage extends React.Component {
                             <li className="nav-item  md-inline-block">
                                 <Link to='/home/FAQ' className='nav-link'>FAQ</Link>
                             </li>
+                             <li className="nav-item  md-inline-block">
+                                <Link to='/home/FAQ' className='nav-link'>FAQ</Link>
+                            </li>
                         </ul>
                     </nav>
                     <Switch>
@@ -49,7 +52,15 @@ export default class HomePage extends React.Component {
                         <Route path="/home/FAQ" component={FAQ} />
                     </Switch>
                 </Router>
-
+                {/* <div className="headimg">
+                <img src={headimg} width="1300" height="550"/>        
+            </div>
+            <div className='div2'>
+                    <p style={{textAlign:'center', fontWeight:'bold'}}>Let clients schedule therapy appointments online with ease</p>
+                    <p style={{textAlign:'center'}}>With Art Aid clients can schedule appointments in just seconds at any convenient time. The schedule allows you to add therapist, also offer different services in one schedule. Would you like to synchronize your online appointment schedule with your private agenda to make sure no therapy appointments can be booked when you have a private appointment? No problem ..................    
+                    We are here!</p>
+            </div> */}
+            
             </div>
 
         );
